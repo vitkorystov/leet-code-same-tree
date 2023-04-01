ba078d22a15ab63cff6363ac9425b3b670b13f84
vitaly_korystov <korystov@inbox.ru>
2023-04-01 20:03:35 +0300

    symmetric-tree problem

--- symmetric-tree.py
+++ symmetric-tree.py
@@ -1,8 +1,8 @@
-# https://leetcode.com/problems/same-tree/
+# https://leetcode.com/problems/symmetric-tree/description/
+
 from typing import Optional
 
 
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -10,8 +10,33 @@ class TreeNode:
         self.right = right
 
 
+tree = TreeNode(
+    val=1,
+    left=TreeNode(val=2,
+                  left=TreeNode(val=3, left=TreeNode(val=5), right=TreeNode(val=6)),
+                  right=TreeNode(val=4, left=TreeNode(val=7), right=TreeNode(val=8)),
+                  ),
+
+    right=TreeNode(val=2,
+                   left=TreeNode(val=4, left=TreeNode(val=8), right=TreeNode(val=7)),
+                   right=TreeNode(val=3, left=TreeNode(val=6), right=TreeNode(val=5)),
+                   ),
+)
+
+
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        left_tree = root.left
+        right_tree = root.right
+
+        if left_tree is None and right_tree is None:
+            return True
+        if left_tree is None and right_tree is not None:
+            return False
+        if left_tree is not None and right_tree is None:
+            return False
+
         def tree_iteration(tree: TreeNode):
             is_level_0 = True
             level_nodes = []
@@ -32,7 +57,11 @@ class Solution:
                 if len(set(level_nodes)) == 1 and level_nodes[0] is None:
                     break
 
-        for i1, i2 in zip(tree_iteration(p), tree_iteration(q)):
-            if i1 != i2:
+        for i1, i2 in zip(tree_iteration(left_tree), tree_iteration(right_tree)):
+            if i1 != list(reversed(i2)):
                 return False
         return True
+
+
+res = Solution().isSymmetric(tree)
+print(res)  # True
