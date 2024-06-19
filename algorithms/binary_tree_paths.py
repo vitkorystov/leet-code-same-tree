a6474ecd138addd10a1589481c467f10d0cb977e
vkorystov <v.korystov@vk.team>
2024-06-19 22:30:35 +0300

    task: binary-tree-paths

---
+++ algorithms/binary_tree_paths.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/binary-tree-paths/
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
+
+        def rec(tree, s=''):
+            if tree:
+                new_s = s + str(tree.val) + '->'
+                rec(tree.left, new_s)
+                rec(tree.right, new_s)
+                if tree.left is None and tree.right is None:
+                    res.append(s + str(tree.val))
+
+        rec(root)
+        return res
+
+
+print(Solution().binaryTreePaths(
+    TreeNode(
+        1,
+        left=TreeNode(2, left=TreeNode(4)),
+        right=TreeNode(3, left=TreeNode(6), right=TreeNode(7))
+        )
+    )
+)
